08d7b25605c9b09aed46f2b5f03fe7f37789b69c
grzegorzmusial <36244857+grzegorzmusial@users.noreply.github.com>
2018-02-07 21:52:48 +0100

    Initial commit

---
+++ .svn/pristine/4c/4cbcd73ebfe87d288a193be03b79cef1feb427e7.svn-base
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabeSynchronizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bookPath = @"c:\prj\SabeData\Atlantis Gene.epub";
+            var recognizedPath = @"c:\prj\SabeData\ch1recognized.txt";
+
+            var book = new Reader().ReadEpub(bookPath);
+            var recognizedlines = File.ReadAllLines(recognizedPath);
+
+            foreach (var recognizedLine in recognizedlines)
+            {
+                CompareRecognizedLineWithOriginal(recognizedLine);
+            }
+        }
+
+        static int bookLinesIndex;
+
+        private static void CompareRecognizedLineWithOriginal(string recognizedLine)
+        {
+            //var bookline = bookLines[bookLinesIndex];
+
+
+            bookLinesIndex++;
+        }
+    }
+}
---
+++ SabeSynchronizer/Reader.cs
@@ -0,0 +1,43 @@
+﻿using eBdb.EpubReader;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SabeSynchronizer
+{
+    public class Reader
+    {
+        public List<string> ReadEpub(string path)
+        {
+            //Init epub object.
+            Epub epub = new Epub(path);
+            List<NavPoint> navPoints = epub.TOC;
+            foreach (var item in epub.TOC)
+            {
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(item.ContentData.Content);
+                //var doc = new HtmlDocument().LoadHtml(item.ContentData.Content);
+
+                //var doc = new HtmlDocument();
+                //doc.Load(path);
+
+                // With XPath	
+                var pNodes = doc.DocumentNode
+                .SelectNodes("//p");
+
+                foreach (var node in pNodes)
+                {
+                    Console.WriteLine(node.OuterHtml);
+                }
+                //// With LINQ	
+                //var nodes = doc.DocumentNode.Descendants("input")
+                //.Select(y => y.Descendants()
+                //.Where(x => x.Attributes["class"].Value == "box"))
+                //.ToList();
+            }
+
+            return new List<string> { "asdf" };
+        }
+    }
+}
